fa817617dc7d2413eaf1e0a51f183c38501f5058
Akilesh Kamble <areyyrahull@gmail.com>
2026-08-26 11:25:52 +0530

    Update vite.config.js

--- my-react-preview/vite.config.js
+++ my-react-preview/vite.config.js
@@ -1,37 +1,33 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-   Sitemap({ 
-      hostname: 'https://speedmock.me', // Replace with your live URL
-      dynamicRoutes: ['/about', '/contact', '/services'] // Add your pages here
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://speedmock.me',
+      dynamicRoutes: ['/about', '/contact', '/services']
     }),
-     
-server: {
-  host: "0.0.0.0",
-  port: 5173,
-  strictPort: true,
-  allowedHosts: true,
+  ],
 
-  hmr: {
-    protocol: "wss",
-    host: "tubular-unlighted-tingle.ngrok-free.dev",
-    clientPort: 443,
-  },
-  
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,
 
-   
-  proxy: {
-    "/api": {
-      target: "http://localhost:5000",
-      changeOrigin: true,
+    hmr: {
+      protocol: "wss",
+      host: "tubular-unlighted-tingle.ngrok-free.dev",
+      clientPort: 443,
+    },
+
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
     },
   },
-},
 })
