95209016e1a8bb3d6de37cf2c574ef726767b8c1
Akilesh Kamble <areyyrahull@gmail.com>
2026-08-26 11:01:27 +0530

    Add Sitemap plugin to Vite configuration

--- my-react-preview/vite.config.js
+++ my-react-preview/vite.config.js
@@ -1,7 +1,17 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
+export default defineConfig({
+  plugins: [
+    react(),
+    Sitemap({ 
+      hostname: 'https://speedmock.me', // Replace with your live URL
+      dynamicRoutes: ['/about', '/contact', '/services'] // Add your pages here
+    }),
+  ],
+})
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
