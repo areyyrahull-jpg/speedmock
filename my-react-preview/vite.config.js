a954441b7289858852f8d0fd08c213b0a02b33c8
Akilesh Kamble <areyyrahull@gmail.com>
2026-08-30 18:33:16 +0530

    Remove vite-plugin-sitemap from Vite config
    Removed sitemap plugin configuration from Vite.

--- my-react-preview/vite.config.js
+++ my-react-preview/vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
-    react(),
-    Sitemap({
-      hostname: 'https://speedmock.me',
-      dynamicRoutes: ['/about', '/contact', '/services']
-    }),
+    react()
   ],
 
   server: {
